b34e0b10faa1b1c6f358c04af2928bbabee7cdf4
Dulyarat455 <Dulyarat.455@gmail.com>
2023-07-13 15:14:43 +0700

    edit

--- src/pages/money.js
+++ src/pages/money.js
@@ -1,17 +1,73 @@
 
-import React from "react";
+import React,{useState} from "react";
 import Nabar from "../../component/Navbar";
+import DropdownSelect from "../../component/Dropdown";
 
 
 export default function Money() {
+  const [selectedIn, setSelectedIn] = useState(0);
+  const [selectedOut, setSelectedOut] = useState(0);
 
+  const options = [
+    { value: 34.64 , label: 'USD-ดอลลาร์สหรัฐ-สหรัฐอเมริกา' },
+    { value: 43.8888, label: 'GBP-ปอนด์สเตอร์ลิง-สหราชอาณาจักร'},
+    { value: 1, label: 'THB-บาท-ไทย' },
+  ];
+  
+    const handleSelectChangeIn =async (event) => {
+      console.log('Selected value:', event.target.value);
+      setSelectedIn(event.target.value)
+      
+    };
+    const handleSelectChangeOut =async (event) => {
+      console.log('Selected value:', event.target.value);
+      setSelectedOut(event.target.value)
+      
+    };
 
 
 return(
 
 <div>
-    <Nabar/>
-    <h1>money</h1>
+          <Nabar/>
+          <br/>
+          <div class="flex justify-center">
+        <div class="bg-blue-500 w-21 px-1 py-1 border-2 border-black-500 rounded flex items-center">
+          <h1 class="text-white ml-1">Money</h1>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 35 20" stroke-width="2.0" stroke="currentColor" class="w-8 h-8 ml-1">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+        </div>
+        
+
+      </div>
+      <br/>
+      <div class="flex justify-center">
+        <h1 className="text-blue-400 mr-1">Input : </h1>
+      <input type="number" className="border-2 border-black-500 rounded"/>
+
+      </div>
+      <br/>
+
+      <div class="flex justify-center">
+      <DropdownSelect options={options} onChange={handleSelectChangeIn} />
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-2 mr-2">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
+      </svg>
+      <DropdownSelect options={options} onChange={handleSelectChangeOut} />
+
+      </div>
+      <br/>
+      <div class="flex justify-center">
+        
+      <button  type='button' className='bg-blue-500 text-white  border-2 m-2 p-2 border-black-500 rounded-lg' >Change</button>
+      </div>
+      <br/>
+
+      <div class="flex justify-center">
+        
+        <h1 className="text-blue-400">result :</h1>
+      </div>
 </div>
 
 
--- src/pages/temperature.js
+++ src/pages/temperature.js
@@ -10,11 +10,20 @@ return(
 <div>
 
 <Nabar/>
-<h1>Temperature</h1>
+<br/>
+<div class="flex justify-center">
+  <div class="bg-blue-500 w-21 px-1 py-1 border-2 border-black-500 rounded flex items-center">
+    <h1 class="text-white ml-1">Temperature</h1>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 35 20" stroke-width="2.0" stroke="currentColor" class="w-8 h-8 ml-1">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M1 7h5m4 0h5m4 0h5M3 12h6m4 0h6m-16 4h5m4 0h5m4 0h5" />
+    </svg>
+  </div>
+
+
 </div>
 
 
-
+</div>
 
 
 )
