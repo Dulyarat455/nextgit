c0be569f2411e36705b76d0fdfce6e3096d0134d
Dulyarat455 <Dulyarat.455@gmail.com>
2023-07-13 16:13:51 +0700

    edit

--- src/pages/money.js
+++ src/pages/money.js
@@ -12,9 +12,43 @@ export default function Money() {
 
 
   const options = [
+    { value: 0 , label: 'Choose' },
     { value: 34.64 , label: 'USD-ดอลลาร์สหรัฐ-สหรัฐอเมริกา' },
     { value: 43.8888, label: 'GBP-ปอนด์สเตอร์ลิง-สหราชอาณาจักร'},
-    { value: 1, label: 'THB-บาท-ไทย' },
+    { value: 1, label: 'THB-บาท-ไทย'},
+    { value: 38.4015, label: 'EUR-ยูโร-ยูโรโซน' },
+    { value: 0.249398, label: 'JPY-เยน-ญี่ปุ่น' },
+    { value: 4.4860, label: 'HKD-ดอลลาร์ฮ่องกง-ฮ่องกง' },
+    { value: 7.6130, label: 'MYR-ริงกิต-มาเลเซีย' },
+    { value: 26.2356, label: 'SGD-ดอลลาร์สิงคโปร์-สิงคโปร์' },
+    { value: 26.3312, label: 'BND-ดอลลาร์บรูไน-บรูไนดารุสซาลาม' },
+    { value: 0.6378, label: 'PHP-เปโซฟิลิปปินส์-ฟิลิปปินส์' },
+    { value: 0.002424, label: 'IDR-รูเปียอินโดนีเซีย-อินโดนีเซีย' },
+    { value: 0.4531, label: 'INR-รูปีอินเดีย-อินเดีย' },
+    { value: 39.2251, label: 'CHF-ฟรังก์-สวิตเซอร์แลนด์' },
+    { value: 24.1385, label: 'AUD-ดอลลาร์ออสเตรเลีย-ออสเตรเลีย' },
+    { value: 21.8915, label: 'NZD-ดอลลาร์นิวซีแลนด์-นิวซีแลนด์' },
+    { value: 26.5959, label: 'CAD-ดอลลาร์แคนาดา-แคนาดา' },
+    { value: 3.2855, label: 'SEK-โครนา-สวีเดน' },
+    { value: 5.1553, label: 'DKK-โครน-เดนมาร์ก' },
+    { value: 3.3069, label: 'NOK-โครน-นอร์เวย์' },
+    { value: 4.9227, label: 'CNY-เรนมินบิ-จีน' },
+    { value: 2.0403, label: 'MXN-เปโซเม็กซิโก-เม็กซิโก' },
+    { value: 1.9139, label: 'ZAR-แรนด์-แอฟริกาใต้' },
+    { value: 0.0272, label: 'KRW-วอน-เกาหลีใต้'},
+    { value: 1.1334, label: 'TWD-ดอลลาร์ไต้หวัน-ไต้หวัน' },
+    { value: 113.8037, label: 'KWD-ดีนาร์คูเวต-คูเวต' },
+    { value: 9.3160, label: 'SAR-เรียลซาอุดีอาระเบีย-ซาอุดีอาระเบีย' },
+    { value: 9.5132, label: 'AED-ดีแรห์น-สหรัฐอาหรับเอมิเรตส์' },
+    { value: 0.0166, label: 'MMK-จัด-พม่า' },
+    { value: 0.3228, label: 'BDT-ตากา-บังกลาเทศ' },
+    { value: 1.6098, label: 'CZK-คราวน์-สาธารณรัฐเช็ก' },
+    { value: 0.008476, label: 'KHR-เรียลกัมพูชา-กัมพูชา' },
+    { value: 0.001903, label: 'LAK-กีบ-ลาว' },
+    { value: 0.4176, label: 'RUB-รูเบิล-รัสเซีย' },
+    { value: 0.001485, label: 'VND-ดอง-เวียดนาม' },
+    { value: 1.1308, label: 'EGP-ปอนด์-อียิปต์' },
+
   ];
   
     const handleSelectChangeIn = (event) => {
@@ -30,14 +64,17 @@ export default function Money() {
     const Calculate  = () => {
       console.log("selectedIn = ",selectedIn)
       console.log("selectedOut = ",selectedOut)
-      if(selectedIn !== 0 && selectedOut !== 0){
+      if((selectedIn !== 0 && selectedOut !== 0) && inputMoney){
 
+        setResultMoney((inputMoney)*selectedIn/selectedOut)
+      }
+      else if(selectedIn !== 0 && selectedOut !== 0){
         setResultMoney(selectedIn/selectedOut)
       }
       console.log("resultMoney = ",resultMoney)
       
     };
-
+    console.log(inputMoney)
 
 return(
 
@@ -57,7 +94,7 @@ return(
       <br/>
       <div class="flex justify-center">
         <h1 className="text-blue-400 mr-1">Input : </h1>
-      <input type="number" className="border-2 border-black-500 rounded"/>
+      <input type="number" className="border-2 border-black-500 rounded" onChange={(e)=>(setInputMoney(parseFloat(e.target.value)))}/>
 
       </div>
       <br/>
@@ -74,14 +111,15 @@ return(
       <div class="flex justify-center">
         
       <button  type='button' className='bg-blue-500 text-white  border-2 m-2 p-2 border-black-500 rounded-lg'onClick={()=>Calculate()} >Change</button>
+      {resultMoney !== 0 && (<h1 className="text-blue-400 py-4">result : {resultMoney}</h1>)
+}      
       </div>
       <br/>
 
-      <div class="flex justify-center">
+      {/* <div class="flex justify-center">
         
-        {resultMoney !== 0 && (<h1 className="text-blue-400">result : {resultMoney}</h1>)
-}      
-      </div>
+       
+      </div> */}
 </div>
 
 
