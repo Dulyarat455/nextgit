c96dc9bee11b6a634523aae141ebdbd5c6ab0610
Dulyarat455 <Dulyarat.455@gmail.com>
2023-07-20 22:47:12 +0700

    edit

--- src/pages/physics.js
+++ src/pages/physics.js
@@ -1,9 +1,46 @@
 
-import React from "react";
-import Nabar from "../../component/Navbar";
 
+import Nabar from "../../component/Navbar";
+import DropdownSelect from "../../component/Dropdown";
+import React,{useState} from "react";
 
 export default function Physics() {
+  const [selectedIn, setSelectedIn] = useState(0);
+  const [selectedOut, setSelectedOut] = useState(0);
+  const [resultTemp, setResultTemp] = useState(0);
+  const [inputTemp,setInputTemp] = useState(0);
+  const[wran,setWarn] = useState(false)
+  const[warnOp, setWarnOp] = useState(true)
+  const[warnInput, setWarnInput] = useState(true)
+
+
+  const options = [
+    { value: 0 , label:  ' °?' },
+    { value: 1 , label:  ' °C' },
+    { value: 2 , label: '°F' },
+    { value: 3 , label: '°R' },
+    { value: 4 , label: '°k' },
+    
+    
+
+  ];
+
+
+
+  const handleSelectChangeIn = (event) => {
+    console.log('Selected value:', event.target.value);
+    setSelectedIn(parseFloat(event.target.value))
+    
+  };
+  const handleSelectChangeOut = (event) => {
+    console.log('Selected value:', event.target.value);
+    setSelectedOut(parseFloat(event.target.value))
+    
+  };
+  const Calculate  = () => {
+
+
+  }
 
 
 
@@ -20,6 +57,32 @@ return(
     </svg>
   </div>
 </div>
+<br/>
+<div class="flex justify-center">
+        <h1 className="text-blue-400 mr-1">Input : </h1>
+      <input type="number" className="border-2 border-black-500 rounded" onChange={(e)=>(setInputTemp(parseFloat(e.target.value)))}/>
+ </div>
+      <br/>
+
+<div class="flex justify-center">
+    <DropdownSelect options={options} onChange={handleSelectChangeIn} />
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-6 h-6 ml-2 mr-2">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
+    </svg>
+    <DropdownSelect options={options} onChange={handleSelectChangeOut} />
+
+</div>
+<br/>
+      <div class="flex justify-center">
+        
+      <button  type='button' className='bg-blue-500 text-white  border-2 m-2 p-2 border-black-500 rounded-lg'onClick={()=>Calculate()} >Change</button>
+      {resultTemp !== 0 && (<h1 className="text-blue-400 py-4">result : {resultTemp}</h1>)}
+      {wran === false && (<h1 className="text-red-400 py-4">Please select all options and add data in tag input.</h1>)}
+      {warnInput === false && (<h1 className="text-red-400 py-4">Please  add data in tag input.</h1>)}
+      {warnOp === false && (<h1 className="text-red-400 py-4">Please  select all options.</h1>)}
+     
+    
+      </div>
 
 
 
---
+++ src/pages/qrcode.js
@@ -0,0 +1,54 @@
+import React,{useEffect,useState} from "react"
+import { GenerateQRCode } from "../../utils/generateQr";
+import Nabar from "../../component/Navbar";
+
+export default function Qrcode() {
+    const [qrCodeData, setQRCodeData] = useState('');
+    const [inputData, setInputData] = useState('');
+
+
+
+
+    const handleInputChange = (event) => {
+        setInputData(event.target.value);
+      };
+    
+      const handleGenerateQRCode = async () => {
+        const qrCodeDataUrl = await GenerateQRCode(inputData);
+        setQRCodeData(qrCodeDataUrl);
+      };
+
+return(
+    <div>
+        <Nabar/>
+        <br/>
+        <div class="flex justify-center">
+        <h1>QR Code Generator</h1>
+      <input  className="border-2 border-black-500 rounded ml-2" type="text" value={inputData} onChange={handleInputChange} />
+    
+      
+        </div>
+        <br/>
+        <div class="flex justify-center">
+                 <button  className='bg-blue-500 text-white  border-2 m-2 p-2 border-black-500 rounded-lg' onClick={handleGenerateQRCode}>Generate QR Code</button>
+      
+       </div>
+       <div class="flex justify-center">
+
+                {qrCodeData && (
+                    <div>
+                    <img src={qrCodeData} alt="QR Code" />
+                    <br/>
+                    <a className='bg-blue-500 text-white  border-2 m-2 p-2 border-black-500 rounded-lg' href={qrCodeData} download="qrcode.png">
+                        Download QR Code
+                    </a>
+                    </div>
+                )}
+
+       </div>
+    </div>
+
+
+);
+
+}
